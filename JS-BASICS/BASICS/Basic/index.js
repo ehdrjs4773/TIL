58c2fe331d4f96fc743906d17f57e9c4958548ee
ehdrjs4773 <35477058+ehdrjs4773@users.noreply.github.com>
2021-03-09 17:14:38 +0900

    03-09 Update
     TIL 내부 파일 정리.
     starbucks  clone 작업중.

---
+++ JS-BASICS/BASICS/Basic/index.js
@@ -0,0 +1,37 @@
+let myName = "HEROPY";
+let email = "thesecon@gmail.com";
+let hello = `hello ${myName}?!`;
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+
+let True = true; //참
+let False = false; //거짓
+
+//Undefined 값이 할당되지 않은 상태를 나타냅니다.
+let undef;
+let obj = { abc: 123 }; //배열 인가?
+
+console.log(undef);
+console.log(obj.abc);
+
+//Object 객체 데이터 여러 데이터를 Key:Value 형태로 저장한다.
+let user = {
+  name: "HEROPY",
+  age: 85,
+  isVaild: true,
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.isVaild);
+
+//Array(배열 데이터) [] <- 사용한다.
+let fruits = ["Apple", "Banana", "Cherry"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+let myName = "HEROPY";
